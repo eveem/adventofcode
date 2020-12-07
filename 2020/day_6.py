f1820cfa1a9a890b50c3f838cdfd172cc9c814af
eveem <even_narak@hotmail.coml>
2020-12-07 13:27:40 +0700

    add day 6 path 2

--- 2020/day_6.py
+++ 2020/day_6.py
@@ -3,6 +3,16 @@ s = open("input.txt", "r")
 counter = 0
 
 for i in s.read().split("\n\n"):
-    counter += len(set(c for c in i.replace("\n", "")))
+    # counter += len(set(c for c in i.replace("\n", "")))
+
+    temp = {}
+    checker = len(i.split("\n"))
+    for c in i.replace("\n", ""):
+        if c not in temp:
+            temp[c] = 1
+        else:
+            temp[c] += 1
+
+    counter += sum(1 for x in temp.values() if x == checker)
 
 print(counter)
